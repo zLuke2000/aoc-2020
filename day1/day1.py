c0e2bc4a4cebf1e6b5abeeb88049c4b435259c3c
zLuke2000 <75306717+zLuke2000@users.noreply.github.com>
2020-12-01 17:50:17 +0100

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,35 @@
+""" PARTE COMUNE """
+
+inputNum = []
+temp = f.read()
+temp = temp.split("\n")
+f.close()
+
+""" PARTE UNO"""
+again = True
+
+for i in temp:
+    if(again):
+        for j in temp:  
+            if(again):
+                tempI = int(i)
+                tempJ = int(j)
+                if(tempI + tempJ == 2020):
+                    print(tempI*tempJ)
+                    again = False
+
+""" PARTE DUE"""
+again = True
+
+for i in temp:
+    if(again):
+        for j in temp:  
+            if(again):
+                for k in temp:
+                    if(again):
+                        tempI = int(i)
+                        tempJ = int(j)
+                        tempK = int(k)
+                        if(tempI + tempJ + tempK == 2020):
+                            print(tempI*tempJ*tempK)
+                            again = False
