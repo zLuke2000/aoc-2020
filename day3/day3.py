3fc74d5cf5f72badf4d128484e40f028461b187f
zLuke2000 <luca100.lc@gmail.com>
2020-12-03 12:35:54 +0100

    day3 - completo

--- day3/day3.py
+++ day3/day3.py
@@ -15,3 +15,26 @@ for i in temp:
         countAlberi += 1
     pos = (pos+X_OFFSET)%len(i)
 print("Totale alberi incontrati: " + str(countAlberi))
+
+
+""" PARTE DUE """
+countAlberi = [0,0,0,0,0]
+X_OFFSET = 1
+for j in range (4):
+    pos = 0
+    for i in temp:
+        if i[pos] == '#':
+            countAlberi[j] += 1
+        pos = (pos+X_OFFSET)%len(i)
+    X_OFFSET += 2
+
+even = 1
+for i in temp:
+    X_OFFSET = 1
+    if(even%2):
+        if i[pos] == '#':
+            countAlberi[4] += 1
+        pos = (pos+X_OFFSET)%len(i)
+    even += 1
+
+print("Totale alberi incontrati: " + str(countAlberi[0]*countAlberi[1]*countAlberi[2]*countAlberi[3]*countAlberi[4]))
