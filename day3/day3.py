8631c9908c75f91db6bad6ace1741fb1e228c001
zLuke2000 <luca100.lc@gmail.com>
2020-12-03 12:21:34 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -12,9 +12,6 @@ countAlberi = 0
 X_OFFSET = 3
 for i in temp:
     if i[pos] == '#':
-        print("Sentiero: "+ i + "Trovato un albero in posizione: " + str(pos))
         countAlberi += 1
-    else:
-        print("Sentiero: "+ i + "Nessun albero in posizione: " + str(pos))
     pos = (pos+X_OFFSET)%len(i)
 print("Totale alberi incontrati: " + str(countAlberi))
